94910448df0357b0b49510a1da8becb1a50e96f1
CitSonCR <30397637+CitSonCR@users.noreply.github.com>
2018-09-17 21:24:42 -0600

    Lab01
    Lab01

---
+++ Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using Lab01.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person = new Person();
+
+            person.Id = 1;
+            person.Name = "Juan";
+            person.LastName = "Perez";
+
+            ILogger logger = person;
+
+            System.Console.WriteLine("Person");
+
+            string personString = string.Format("Id:{0},Name:{1},LastName:{2}", person.Id, person.Name, person.LastName);
+
+            
+
+            System.Console.WriteLine(personString);
+
+            logger.Log("Hello World");
+
+            System.Console.ReadKey();
+            
+        }
+    }
+}
